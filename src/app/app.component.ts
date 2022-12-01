0b159074a0af1a9c66ed7054f1d234a40d4bf817
sabneks95 <95006362+sabneks95@users.noreply.github.com>
2022-12-01 13:57:02 +0530

    QR Generator code commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from 'ngx-qrcode2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QRCodeGeneratorApp';
+  name = 'Angular' + VERSION.major;
+  elementType = NgxQrcodeElementTypes.URL;
+  correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+  value = 'http://www.youtube.com/c/TestyCodiez';
 }
